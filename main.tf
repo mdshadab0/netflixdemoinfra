44beae2e547a0ef7868740a848bcb43dd3446c93
Mohammed Shadab <mdshadab0500@gmail.com>
2025-09-03 18:33:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 provider "aws" {
-  region = "ap-south-1b"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
   count                  = 4
   ami                    = "ami-02d26659fd82cf299"
+  availability_zone      = "ap-south-1b"
   instance_type          = "t2.medium"
   key_name               = "Auto"
   vpc_security_group_ids = ["sg-07ede7518dd4357a0"]
